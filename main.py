dbba2e2c645806e600f597d4e788f736a3a1a708
xstop-yoyo <109899284+xstop-yoyo@users.noreply.github.com>
2023-01-11 17:13:21 +0000

    Bob

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-import turtle
-draw = turtle.Turtle()
-draw.speed(0)
+import pygame, sys
+
+pygame.init()
+
+pygame.display.set_mode((1280, 720))
+
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    clock.tick(60)
